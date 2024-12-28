6edfa440eb16d6f8fc692b0cee6a6fd579e50dfa
PaataSet <paataseturidze@gmail.com>
2024-12-28 15:35:00 +0400

    add section-1
    add html, css and javascript (slider) for section-1.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,32 @@
 console.log("Final Project");
+
+const slides = document.querySelectorAll(".slide");
+
+const slideFn = () => {
+  let currentSlide = 0;
+  slides[currentSlide].classList.add("active");
+
+  const showSlides = () => {
+    slides.forEach((slide, index) => {
+      if (index === currentSlide) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+  };
+
+  const goToNextSlide = () => {
+    if (currentSlide === slides.length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    showSlides();
+  };
+
+  const slideInterval = setInterval(goToNextSlide, 2000);
+  showSlides();
+};
+
+slideFn();
