7a4edd8984661e42384d74cf455418c05ea1d59d
PaataSet <paataseturidze@gmail.com>
2025-01-09 18:41:31 +0400

    add styling
    add latest projects styles, round slides.
    some more styling and html code

--- src/js/app.js
+++ src/js/app.js
@@ -25,7 +25,7 @@ const slideFn = () => {
     showSlides();
   };
 
-  const slideInterval = setInterval(goToNextSlide, 2000);
+  const slideInterval = setInterval(goToNextSlide, 5000);
   showSlides();
 };
 
@@ -46,3 +46,38 @@ window.onscroll = function () {
 };
 
 // end
+
+// დასაწყისი
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("rs-slide");
+  let dots = document.getElementsByClassName("rs-button");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" rs-button2", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " rs-button2";
+}
+
+// დასასრული
