a2fce0e075b2214efe0d3be6496ef016f0a7e335
PaataSet <paataseturidze@gmail.com>
2025-01-13 20:18:23 +0400

    add filter for Latest Projects

--- src/js/app.js
+++ src/js/app.js
@@ -81,3 +81,67 @@ function showSlides(n) {
 }
 
 // დასასრული
+
+// ფილტრის დასაწყისი
+filterSelection("all");
+function filterSelection(c) {
+  let x, i;
+  x = document.getElementsByClassName("lp-card");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+let btnContainer = document.getElementsById("nav-div");
+let btns = btnContainer.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName(" active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// ფილტრის დასასრული
+
+// დასაწყისი
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// დასასრული
